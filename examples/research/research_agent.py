7fb4bb8c287955a45ad85cec640579ddf4461364
Harrison Chase <hw.chase.17@gmail.com>
2025-07-29 07:45:38 -0700

    cr

--- examples/research/research_agent.py
+++ examples/research/research_agent.py
@@ -45,7 +45,17 @@ The user may ask for specific areas to critique the report in. Respond to the us
 
 You can use the search tool to search for information, if that will help you critique the report
 
-Do not write to the `final_report.md` yourself."""
+Do not write to the `final_report.md` yourself.
+
+Things to check:
+- Check that each section is appropriately named
+- Check that the report is written as you would find in an essay or a textbook - it should be text heavy, do not let it just be a list of bullet points!
+- Check that the report is comprehensive. If any paragraphs or sections are short, or missing important details, point it out.
+- Check that the article covers key areas of the industry, ensures overall understanding, and does not omit important parts.
+- Check that the article deeply analyzes causes, impacts, and trends, providing valuable insights
+- Check that the article closely follows the research topic and directly answers questions
+- Check that the article has a clear structure, fluent language, and is easy to understand.
+"""
 
 critique_sub_agent = {
     "name": "critique-agent",
--- src/claude_everything/state.py
+++ src/claude_everything/state.py
@@ -1,5 +1,5 @@
 from langgraph.prebuilt.chat_agent_executor import AgentState
-from typing import NotRequired
+from typing import NotRequired, Annotated
 from typing import Literal
 from typing_extensions import TypedDict
 
@@ -11,6 +11,16 @@ class Todo(TypedDict):
     status: Literal["pending", "in_progress", "completed"]
 
 
+
+def file_reducer(l, r):
+    if l is None:
+        return r
+    elif r is None:
+        return l
+    else:
+        return {**l, **r}
+
+
 class DeepAgentState(AgentState):
     todos: NotRequired[list[Todo]]
-    files: NotRequired[dict[str, str]]
+    files: Annotated[NotRequired[dict[str, str]], file_reducer]
