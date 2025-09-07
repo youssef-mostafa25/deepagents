c376bc240cbfb5a67354cd12143739efa2e2721f
youssef-mostafa25 <youssef.mostafa@pixonal.com>
2025-09-07 16:37:23 +0300

    fix: allow localhost 3000 to send requests

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 from examples.research.research_agent import agent
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class InputBaseModel(BaseModel):
     user_input: str
